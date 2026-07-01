5875abd56eeff53163166c66d03658b6525e1e5e
Nathan Kerns <nmk456@users.noreply.github.com>
2026-06-30 22:36:43 -0700

    Add sensitivity analysis

--- sim/sim_linear.py
+++ sim/sim_linear.py
@@ -6,12 +6,12 @@ from PyLTSpice import SimRunner, AscEditor, RawRead
 from spicelib.editor.base_schematic import TextTypeEnum
 from pathlib import Path
 from matplotlib import pyplot as plt
+from threading import Lock
 import numpy as np
 import shutil
 import time
 import re
-
-# TODO: run sims in parallel, use callbacks
+import json
 
 ANALYSIS_REGEX = [r"\.tran.*", r"\.fra.*"]
 
@@ -54,28 +54,21 @@ def remove_Xinstruction_quiet(netlist: AscEditor, pattern: str):
         return False
 
 
-def print_fra_results(results: dict):
-    print(f"Crossover frequency: {results['gain_crossover'] / 1e3:.4g} kHz")
-    print(f"Phase margin: {np.rad2deg(results['phi_margin']):.4g} deg")
-    print()
-
-
-def print_ripple_results(results: dict):
-    print(f"Vppk(OUT): {results['Vppk(OUT)'] * 1e3:0.4g} mV")
-    print(f"Vppk(FILT): {results['Vppk(FILT)'] * 1e3:0.4g} mV")
-    print()
-
-
 class Sim:
     def __init__(
         self, source: str, parallel_sims: int, output_folder: str, cached: bool = False
     ):
         self.source = Path(source)
         self.cached = cached
-        self.ltc = SimRunner(parallel_sims=parallel_sims, output_folder=output_folder)
+        self.ltc = SimRunner(
+            parallel_sims=parallel_sims, timeout=180, output_folder=output_folder
+        )
         copy_libs(self.ltc)
 
         self.results = {}
+        self.results_log = []
+
+        self.plot_lock = Lock()
 
     def get_netlist(self):
         netlist = AscEditor(self.source)
@@ -116,13 +109,17 @@ class Sim:
         self.ltc.run(
             netlist, callback=callback, callback_args=args, run_filename=f"{name}.asc"
         )
+        netlist.remove_instruction(cmd)
+
+        self.results[name] = {
+            "cmd": cmd,
+            "netlist": netlist,
+        }
 
     def start_fra_sim(self, netlist: AscEditor, name: str = "fra"):
         self.start_sim_callback(netlist, name, ".fra startup", self.fra_sim_callback)
 
     def fra_sim_callback(self, raw_file: Path, log_file: Path, name: str):
-        results = {}
-
         fra_file = str(raw_file).replace(".raw", ".fra_1.raw")
         fra = RawRead(fra_file)
 
@@ -132,38 +129,47 @@ class Sim:
         mag = np.abs(gain)
         phase = np.unwrap(np.angle(gain))
 
-        plt.figure(figsize=(8, 6), dpi=200)
-
-        plt.subplot(2, 1, 1)
-        plt.semilogx(freq.real, 20 * np.log10(mag))
-
-        plt.subplot(2, 1, 2)
-        plt.semilogx(freq.real, np.rad2deg(phase))
-
-        plt.tight_layout()
-        plt.savefig(f"{name}_bode.png")
-
         # Phase margin
-        idx_cg = np.where(mag > 1)[0][-1]
+        idx_cg = np.where(mag < 1)[0][0] - 1
         gain_crossover = np.interp(
-            1, mag[idx_cg : idx_cg + 2], freq[idx_cg : idx_cg + 2]
+            1, mag[idx_cg : idx_cg + 2][::-1], freq[idx_cg : idx_cg + 2][::-1]
         )
         phi_margin = np.interp(gain_crossover, freq, phase)
 
-        results["gain_crossover"] = gain_crossover
-        results["phi_margin"] = phi_margin
+        self.results[name]["gain_crossover"] = gain_crossover
+        self.results[name]["phi_margin"] = np.rad2deg(phi_margin)
 
         # Gain margin
         idx_cp = np.where(phase > 0)[0][-1]
         phase_crossover = np.interp(
-            1, phase[idx_cp : idx_cp + 2], freq[idx_cp : idx_cp + 2]
+            0, phase[idx_cp : idx_cp + 2], freq[idx_cp : idx_cp + 2]
         )
         g_margin = np.interp(phase_crossover, freq, mag)
 
-        results["phase_crossover"] = phase_crossover
-        results["g_margin"] = g_margin
+        self.results[name]["phase_crossover"] = phase_crossover
+        self.results[name]["g_margin"] = g_margin
 
-        print_fra_results(results)
+        with self.plot_lock:
+            plt.figure(figsize=(8, 6), dpi=200)
+
+            plt.subplot(2, 1, 1)
+            plt.semilogx(freq.real, 20 * np.log10(mag))
+            plt.grid()
+
+            plt.subplot(2, 1, 2)
+            plt.semilogx(freq.real, np.rad2deg(phase))
+            plt.grid()
+
+            plt.tight_layout()
+            plt.savefig(f"{name}.png")
+
+        self.results[name]["graph"] = [f"{name}.png"]
+
+        log = f"# {name}\n\n"
+        log += f"Crossover frequency: {self.results[name]['gain_crossover'] / 1e3:.4g} kHz\n"
+        log += f"Phase margin: {self.results[name]['phi_margin']:.4g} deg\n\n"
+
+        self.results_log.append(log)
 
     def start_ripple_sim(self, netlist: AscEditor, name: str = "ripple"):
         self.start_sim_callback(
@@ -171,8 +177,6 @@ class Sim:
         )
 
     def ripple_sim_callback(self, raw_file: Path, log_file: Path, name: str):
-        results = {}
-
         # Speed up loading by only reading needed traces
         raw = RawRead(raw_file, traces_to_read=["time", "V(out_ac)", "V(filt_ac)"])
         t = raw.get_trace("time").get_wave()  # Workaround for compression issues
@@ -185,44 +189,124 @@ class Sim:
         filt_last_ms = filt[i_last_ms:]
         out_last_ms = out[i_last_ms:]
 
-        results["Vppk(OUT)"] = np.abs(np.max(out_last_ms) - np.min(out_last_ms))
-        results["Vppk(FILT)"] = np.abs(np.max(filt_last_ms) - np.min(filt_last_ms))
+        self.results[name]["Vppk(OUT)"] = float(
+            np.abs(np.max(out_last_ms) - np.min(out_last_ms))
+        )
+        self.results[name]["Vppk(FILT)"] = float(
+            np.abs(np.max(filt_last_ms) - np.min(filt_last_ms))
+        )
 
-        plt.figure(figsize=(8, 8), dpi=200)
+        with self.plot_lock:
+            plt.figure(figsize=(8, 8), dpi=200)
 
-        plt.subplot(3, 1, 1)
-        plt.plot(t, out, label="OUT")
-        plt.plot(t, filt, label="FILT")
-        plt.title("Full Span")
-        plt.legend()
+            plt.subplot(3, 1, 1)
+            plt.plot(t, out, label="OUT")
+            plt.plot(t, filt, label="FILT")
+            plt.title("Full Span")
+            plt.legend()
 
-        plt.subplot(3, 1, 2)
-        plt.plot(t_last_ms, out_last_ms)
-        plt.title("OUT (last 1ms)")
+            plt.subplot(3, 1, 2)
+            plt.plot(t_last_ms, out_last_ms)
+            plt.title("OUT (last 1ms)")
 
-        plt.subplot(3, 1, 3)
-        plt.plot(t_last_ms, filt_last_ms)
-        plt.title("FILT (last 1ms)")
+            plt.subplot(3, 1, 3)
+            plt.plot(t_last_ms, filt_last_ms)
+            plt.title("FILT (last 1ms)")
 
-        plt.tight_layout()
-        plt.savefig("ripple.png")
+            plt.tight_layout()
+            plt.savefig(f"{name}.png")
 
-        # TODO: dissipation
+        self.results[name]["graph"] = [f"{name}.png"]
 
-        # return results
-        print_ripple_results(results)
+        log = f"# {name}\n\n"
+        log += f"Vppk(OUT): {self.results[name]['Vppk(OUT)'] * 1e3:0.4g} mV\n"
+        log += f"Vppk(FILT): {self.results[name]['Vppk(FILT)'] * 1e3:0.4g} mV\n\n"
+
+        self.results_log.append(log)
+
+    def log_results(self, file: str):
+        with open(file, "w") as f:
+            for log in self.results_log:
+                f.write(log)
+
+    def log_json(self, file: str):
+        with open(file, "w") as f:
+            f.write(
+                json.dumps(
+                    self.results,
+                    default=lambda o: f"<<non-serializable: {type(o).__qualname__}>>",
+                )
+            )
+
+    def sensitivity_analysis(self, components: list, rel_step=0.01):
+        for component in components:
+            netlist = self.get_netlist()
+
+            value = netlist.get_component_floatvalue(component)
+            value += value * rel_step
+            netlist.set_component_value(component, value)
+
+            self.start_fra_sim(netlist, f"fra_{component}")
+            self.start_ripple_sim(netlist, f"ripple_{component}")
+
+        self.ltc.wait_completion()
+        netlist = self.get_netlist()
+
+        # Partial derivatives
+        # results = {}
+        log = "# Sensitivity Analysis\n\n"
+
+        log += f"Increased each component's value by {rel_step:.3%}\n\n"
+
+        for component in components:
+            try:
+                log += f"{component} ({netlist.get_component_value(component)}):\n"
+
+                results = {}
+
+                params = [
+                    ("fra", "gain_crossover"),
+                    ("fra", "phi_margin"),
+                    ("ripple", "Vppk(OUT)"),
+                    ("ripple", "Vppk(FILT)"),
+                ]
+
+                for sim_type, param in params:
+                    baseline = self.results[f"{sim_type}_baseline"][param]
+                    changed = self.results[f"{sim_type}_{component}"][param]
+
+                    results[f"{param}_delta"] = changed - baseline
+                    results[f"{param}_rel"] = changed / baseline - 1
+
+                log += f"* Gain crossover frequency: {results['gain_crossover_delta']:0.4g} Hz ({results['gain_crossover_rel']:.5%})\n"
+                log += f"* Phase margin: {results['phi_margin_delta']:0.4g} deg ({results['phi_margin_rel']:.5%})\n"
+                log += f"* OUT ripple: {results['Vppk(OUT)_delta'] * 1e3:0.4g} mV ({results['Vppk(OUT)_rel']:.5%})\n"
+                log += f"* FILT ripple: {results['Vppk(FILT)_delta'] * 1e3:0.4g} mV ({results['Vppk(FILT)_rel']:.5%})\n"
+
+                log += "\n"
+            except KeyError:
+                pass
+
+        self.results_log.append(log)
 
 
 def main():
-    cached = False
+    cached = True
 
     start = time.time()
-    sim = Sim("power_supply_linear.asc", 8, "tmp", cached)
+    sim = Sim("power_supply_linear.asc", 4, "tmp", cached)
 
-    sim.start_fra_sim(sim.get_netlist())
-    sim.start_ripple_sim(sim.get_netlist())
+    sim.start_fra_sim(sim.get_netlist(), "fra_baseline")
+    sim.start_ripple_sim(sim.get_netlist(), "ripple_baseline")
+
+    # components = ["C1", "C4"]
+    components = ["C1", "C4", "C5", "R2", "R3", "R14", "R15", "C13", "R4"]
+    sim.sensitivity_analysis(components)
 
     sim.ltc.wait_completion()
+
+    sim.log_results("log.md")
+    sim.log_json("results.json")
 
     end = time.time()
     print(f"Total time: {end - start:0.2f}s")
